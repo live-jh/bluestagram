02f26c4eeee4483ccda63e1412ffd20399615f04
live-jh <livejh7129@gmail.com>
2021-07-27 23:59:05 +0900

    add useAxios

--- frontend/src/components/suggestion/SuggestionList.js
+++ frontend/src/components/suggestion/SuggestionList.js
@@ -1,33 +1,41 @@
 import React, {useEffect, useState} from "react";
-import {Card} from "antd";
+import {Button, Card} from "antd";
 import Suggestion from "./Suggestion";
-import Axios from "axios";
+import useAxios from "axios-hooks";
 import {useAppContext} from "../../store";
 
 export default function SuggestionList({style}) {
-    const [userList, setUserList] = useState([]);
+    // const [userList, setUserList] = useState([]);
     const {store: {jwt_token}} = useAppContext();
+    const headers = {Authorization: `JWT ${jwt_token}`};
+    const [{data: user_list, loading, error}, refetch] = useAxios({
+        url: `${process.env.REACT_APP_API_URL}/api/account/suggestions`,
+        headers: headers
+    })
     useEffect(() => {
-        async function fetchUserList() {
-            const headers = {Authorization: `JWT ${jwt_token}`};
-            try {
-                const {data} = await Axios.get(
-                    `${process.env.REACT_APP_API_URL}/api/account/suggestions`,
-                    {headers}
-                )
-                setUserList(data)
-            } catch (error) {
-                console.log(error);
-            }
-        }
+        // async function fetchUserList() {
+            // try {
+            //     const {data} = await Axios.get(
+            //         `${process.env.REACT_APP_API_URL}/api/account/suggestions`,
+            //         {headers}
+            //     )
+            //     setUserList(data)
+            // } catch (error) {
+            //     console.log(error);
+            // }
+        // }
+        // fetchUserList();
 
-        fetchUserList();
-    // eslint-disable-next-line
+        // eslint-disable-next-line
     }, []);
     return (
         <div style={style}>
+            {loading && <div>...loading</div>}
+            {error && <div>로딩중 에러가 발생하였습니다.</div>}
+            {/* request again call */}
+            {/*<Button onClick={() => refetch()}>Reload</Button>*/}
             <Card title="Suggestion for u" size="small">
-                {userList.map((suggesion_user, index) => {
+                {user_list && user_list.map((suggesion_user, index) => {
                     return (
                         <Suggestion key={index} suggestion_user={suggesion_user}/>
                     )
