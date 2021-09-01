822e648b4ebd2211d47455b4829f76d2dd7c2258
live-jh <livejh7129@gmail.com>
2021-09-01 23:59:31 +0900

    add follow api

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
-from account.views import SignUpAPI, SuggestionListAPI
+from account.views import SignUpAPI, SuggestionListAPI, user_follow, user_unfollow
 
 urlpatterns = [
     path('signup/', SignUpAPI.as_view(), name='signup'),
@@ -8,5 +8,7 @@ urlpatterns = [
     path('token/refresh/', refresh_jwt_token),
     path('token/verify/', verify_jwt_token),
     path('suggestions/', SuggestionListAPI.as_view(), name='suggestion_user_list'),
+    path('follow/', user_follow, name='user_follow'),
+    path('unfollow/', user_unfollow, name='user_unfollow'),
 
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.decorators import api_view
+from rest_framework.generics import CreateAPIView, ListAPIView, get_object_or_404
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
@@ -28,3 +29,22 @@ class SignUpAPI(CreateAPIView):
 class SuggestionListAPI(ListAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = SuggestionUserSerializer
+
+
+# 함수형으로 간단히 구현
+@api_view(['POST'])  # decorator
+def user_follow(request):
+    username = request.data['username']
+    follow_user = get_object_or_404(User, username=username, is_active=True)
+    request.user.following_set.add(follow_user)
+    follow_user.follower_set.add(request.user)
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['POST'])
+def user_unfollow(request):
+    username = request.data['username']
+    follow_user = get_object_or_404(User, username=username, is_active=True)
+    request.user.following_set.remove(follow_user)
+    follow_user.follower_set.remove(request.user)
+    return Response(status=status.HTTP_204_NO_CONTENT)
