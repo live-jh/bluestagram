53ddeca91b3d647ca2cd8e68d8a998f2d089659f
live-jh <livejh7129@gmail.com>
2021-09-04 19:39:35 +0900

    drf 인증 관련 debug_toolbar 옵션 및 author serializer 추가

--- bluestagram/views.py
+++ bluestagram/views.py
@@ -16,22 +16,27 @@ from .models import Post
 
 
 class PostViewSet(ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = (Post.objects.all().select_related('author').prefetch_related('tag_set', 'like_user_set'))
     serializer_class = PostSerializer
 
-    # permission_classes = [AllowAny]
+    # permission_classes = [AllowAny] #인증 적용
 
     # viewset, class 기반 view 에서 get_query_set 재정의 가능
     def get_queryset(self):
-        timesince = timezone.now() - timedelta(days=3)  # 일자 가능
-
+        # timesince = timezone.now() - timedelta(days=3)  # 3일전
+        # qs = qs.filter(created_at__gte=timesince)
         qs = super().get_queryset()  # 로그인 되어 있음을 보장
-        qs = qs.filter(
-            Q(author=self.request.user) |
-            Q(author__in=self.request.user.following_set.all())  # 내가 팔로우 한 목록
-        )
-        # qs = qs.filter(created_at__gte=timesince) # 3일전
+        qs = qs.filter(Q(author=self.request.user) | Q(author__in=self.request.user.following_set.all()))  # 내가 팔로우한 목록
         return qs
+
+    # 저장시 로직
+    def perform_create(self, serializer):
+        # form logic
+        # post = from.save(commit=False)
+        # post.author = self.request.user
+        # post.save()
+        serializer.save(author=self.request.user)
+        return super().perform_create(serializer)
 
 
 # class PostAPI(APIView):
